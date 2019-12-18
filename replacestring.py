23eda31a631f54bfc6be21d86be71471b46d0d79
Jaime Hernandez <jaimevazz@gmail.com>
2019-12-18 08:52:14 -0600

    Removed all coincidences

--- replacestring.py
+++ replacestring.py
@@ -6,19 +6,23 @@ subC = (">>>>>>> Stashed changes\n")
 
 with open(filename, 'r+') as f:
   data = f.read()
+  count = 0
   # Iterate starts
-  # while True:
-
-  # Find indexes
-  start, end = data.index(subA), data.index(subB)
-  # Slice string
-  data = data[:start] + data[end:]
-  data = data.replace(subB,"", 1).replace(subC,"", 1)
-  # Iterate ends
-  # if(data.find(subA) < 0)
-  #   break
+  while True:
+      # Find indexes
+      start, end = data.index(subA), data.index(subB)
+      # Slice string
+      data = data[:start] + data[end:]
+      data = data.replace(subB,"", 1).replace(subC,"", 1)
+      # Iterate ends
+      count += 1
+      if(data.find(subA) < 0):
+      # if(count > 4):
+        break
   # return pointer to top of file so we can re-write the content with replaced string
   f.seek(0)
+  # Deletes
+  f.truncate()
   # re-write the content with the updated content
   f.write(data)
   # close file
