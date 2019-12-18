7f7d0918d2b4a6a85352e66afe0101b029a55784
Jaime Hernandez <jaimevazz@gmail.com>
2019-12-18 09:01:45 -0600

    Select stashed from file

---
+++ selectstashed.py
@@ -0,0 +1,31 @@
+import sys
+program_name = sys.argv[0]
+filename = sys.argv[1]
+
+subA = ("<<<<<<< Updated upstream\n")
+subB = ("=======\n")
+subC = (">>>>>>> Stashed changes\n")
+
+with open(filename, 'r+') as f:
+  data = f.read()
+  count = 0
+  # Iterate starts
+  while True:
+      # Find indexes
+      start, end = data.index(subA), data.index(subB)
+      # Slice string
+      data = data[:start] + data[end:]
+      data = data.replace(subB,"", 1).replace(subC,"", 1)
+      # Iterate ends
+      count += 1
+      if(data.find(subA) < 0):
+      # if(count > 4):
+        break
+  # return pointer to top of file so we can re-write the content with replaced string
+  f.seek(0)
+  # Deletes
+  f.truncate()
+  # re-write the content with the updated content
+  f.write(data)
+  # close file
+  f.close()
